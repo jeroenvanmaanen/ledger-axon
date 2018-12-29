e7b0dd6ea2fee3c9a7a1476dea04d70acc4de018
Jeroen van Maanen <jeroen@leialearns.org>
2018-12-29 10:24:09 +0100

    Added sample data. Added script "convert-compound-export.sh".

---
+++ bin/convert-compound-export.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Convert an export of the compound collection of the old React-only implementation of Ledger that was made
+# with NoSQLBooster for MongoDB using the MongoShell BSON format, to the YAML format that can be imported
+# in the new Axon-based back-end for Ledger.
+
+cat "$@" \
+    | sed -E \
+        -e '/^[}],/a\
+---' \
+        -e '/^[}],/s/,//' \
+        -e 's/NumberInt[(](-?[0-9]*)[)]/\1/g' \
+        -e 's/ObjectId[(]("[^"]*")[)]/\1/g' \
+        -e 's/("amount" *: *)"(-?[0-9]*)"/\1\2/'
