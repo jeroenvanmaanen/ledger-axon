8a90253b94e1e80111200439f3c3966af75a9922
Jeroen van Maanen <jeroen@leialearns.org>
2019-02-13 21:15:19 +0100

    Moved AxonConfig to package "axon". Moved RestConfig to package "rest".

--- core/src/main/java/org/sollunae/ledger/axon/AxonConfig.java
+++ core/src/main/java/org/sollunae/ledger/axon/AxonConfig.java
@@ -1,4 +1,4 @@
-package org.sollunae.ledger.spring;
+package org.sollunae.ledger.axon;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
--- core/src/main/java/org/sollunae/ledger/rest/RestConfig.java
+++ core/src/main/java/org/sollunae/ledger/rest/RestConfig.java
@@ -1,4 +1,4 @@
-package org.sollunae.ledger.spring;
+package org.sollunae.ledger.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
