7e8a4b797586f0a3ea97d96a55b19774b91d5bb7
Jeroen van Maanen <jeroen@leialearns.org>
2019-02-19 13:19:47 +0100

    Added automatic timestamp in generated file name in script "data-backup.sh".

--- bin/data-backup.sh
+++ bin/data-backup.sh
@@ -3,14 +3,19 @@
 BIN="$(cd "$(dirname "$0")" ; pwd)"
 PROJECT="$(dirname "${BIN}")"
 
+source "${BIN}/verbose.sh"
+
 SUFFIX=''
-if [[ -n ".$1" ]]
+if [[ -n "$1" ]]
 then
-    SUFFIX="-$1"
+    SUFFIX="$1"
     shift
+else
+    SUFFIX="$(date '+%Y%m%dT%H%M')"
 fi
+log "SUFFIX=[${SUFFIX}]"
 
 docker run --rm -ti \
     -v ledger_axon-data:/opt/axonframework/data \
     -v "${PROJECT}:${PROJECT}" -w '/opt/axonframework/data' \
-    alpine:latest /bin/sh -c "tar -cvzf - . > '${PROJECT}/data/backup${SUFFIX}.tar.gz'"
+    alpine:latest /bin/sh -c "tar -cvzf - . > '${PROJECT}/data/backup-${SUFFIX}.tar.gz'"
